278ca8580f2f2a9a896df4103ff0a9514492e981
Jinglianwu <ntjinglian@gmail.com>
2018-07-28 22:56:00 +0800

    test

--- application.py
+++ application.py
@@ -5,35 +5,35 @@ from models import db
 
 def say_hello(username = "World"):
     return '<p>Hello %s!</p>\n' % username
-
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
-
-# EB looks for an 'application' callable by default.
-application = Flask(__name__)
-
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
-
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
-
-# run the app.
-if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    application.debug = True
-    application.run()
+#
+# # some bits of text for the page.
+# header_text = '''
+#     <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+# instructions = '''
+#     <p><em>Hint</em>: This is a RESTful web service! Append a username
+#     to the URL (for example: <code>/Thelonious</code>) to say hello to
+#     someone specific.</p>\n'''
+# home_link = '<p><a href="/">Back</a></p>\n'
+# footer_text = '</body>\n</html>'
+#
+# # EB looks for an 'application' callable by default.
+# application = Flask(__name__)
+#
+# # add a rule for the index page.
+# application.add_url_rule('/', 'index', (lambda: header_text +
+#     say_hello() + instructions + footer_text))
+#
+# # add a rule when the page is accessed with a name appended to the site
+# # URL.
+# application.add_url_rule('/<username>', 'hello', (lambda username:
+#     header_text + say_hello(username) + home_link + footer_text))
+#
+# # run the app.
+# if __name__ == "__main__":
+#     # Setting debug to True enables debug output. This line should be
+#     # removed before deploying a production app.
+#     application.debug = True
+#     application.run()
 
 
 
@@ -53,7 +53,7 @@ from utils_non_flask import generate_result_folder_for_pickle, \
     pickle_read_file_direct, \
     unpack_to_dict
 
-application = app = Flask(__name__)
+application = Flask(__name__)
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask:p2s5w0rD@flasktest.cfparnusqsew.us-west-2.rds.amazonaws.com/aeolusdb'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/capstone'
@@ -118,7 +118,7 @@ def requires_auth(f):
 @requires_auth
 def main_auth():
     return "Hello World!", status.HTTP_200_OK
-#
+
 # @application.route("/video", methods=['POST'])
 # @requires_auth
 # def video_upload():
